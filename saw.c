b0b2dcd27ef8a7b27abe53955e6dd49ad7a832ad
CSE <100668735+atp20cs@users.noreply.github.com>
2023-06-12 20:55:46 +0530

    saw.c

--- saw.c
+++ saw.c
@@ -1,30 +1,37 @@
-#include<stdio.h>
-#include<stdlib.h>
-int main()
-{
-	int i,j,n,x,x1=10,x2;
-	for(i=0;i<200;i++)
-		rand();
-	n = rand()/200;
-	i=j=1;
-	n = n/8;
-	printf("\n Number of frames is %d",n);
-	while(n>0)
-	{	
-		printf("\n Sending frame %d",i);
-		srand(x1++);
-		x = rand()%10;
-		if(x%2 == 0)
-		{
-			for(x2 = 1; x2<2 ; x2++)
-				sleep(x2);
-			printf("\n Sending frame %d",i);
-			srand(x1++);
-			x = rand()%10;
-		}
-		printf("\n Ack for frame %d",j);
-		n-=1;
-		i++;j++;
-	}
-	printf("\n End of Stop and Wait protocol");
+#include<stdio.h> 
+
+#include<stdlib.h> 
+void main() 
+{ 
+int i,j,noframes,x,x1=10,x2; 
+
+for(i=0;i<200;i++) 
+
+noframes=rand()/200; 
+i=1; 
+j=1; 
+noframes = noframes / 8; 
+printf("\n number of frames is %d",noframes); 
+while(noframes>0) 
+{ 
+printf("\nsending frame %d",i); 
+srand(x1++); 
+x = rand()%10; 
+if(x%20) 
+{ 
+for (x2=1; x2<2; x2++) 
+{ 
+printf("waiting for %d seconds\n",x2); 
+sleep(x2); 
+printf("\nsending frame %d",i); 
+srand(x1++); 
+x = rand()%10; 
+} 
+printf("\nack for frame %d",j); 
+noframes--; 
+i++; 
+j++; 
+} 
+printf("\n end of stop and wait protocol"); 
 }
+} 
